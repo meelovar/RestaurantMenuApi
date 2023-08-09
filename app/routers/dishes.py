39a9ea603855470fcd6bc5c993a08d8df8cc0d2f
meelovar <104781245+meelovar@users.noreply.github.com>
2023-08-09 21:02:27 +0300

    Changed prefix of dishes and submenus routers

--- app/routers/dishes.py
+++ app/routers/dishes.py
@@ -8,31 +8,29 @@ from app.dependencies import valid_dish
 from app.schemas import DishSchemaIn, DishSchemaOut
 from app.services import DishesService
 
-router = APIRouter(prefix='/menus', tags=['dishes'])
+router = APIRouter(prefix='/menus/{menu_id}/submenus/{submenu_id}/dishes', tags=['dishes'])
 
 
-@router.get('/{menu_id}/submenus/{submenu_id}/dishes', response_model=list[DishSchemaOut])
+@router.get('', response_model=list[DishSchemaOut])
 async def get_list(menu_id: UUID, submenu_id: UUID, dishes_svc: DishesService = Depends()):
     result = await dishes_svc.get_list(menu_id, submenu_id)
 
     return result
 
 
-@router.get('/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}', response_model=DishSchemaOut)
+@router.get('/{dish_id}', response_model=DishSchemaOut)
 async def get(dish: Mapping = Depends(valid_dish)):
     return dish
 
 
-@router.post('/{menu_id}/submenus/{submenu_id}/dishes',
-             response_model=DishSchemaOut,
-             status_code=status.HTTP_201_CREATED)
+@router.post('', response_model=DishSchemaOut, status_code=status.HTTP_201_CREATED)
 async def create(menu_id: UUID, submenu_id: UUID, dish_data: DishSchemaIn, dishes_svc: DishesService = Depends()):
     result = await dishes_svc.create(menu_id, submenu_id, dish_data)
 
     return result
 
 
-@router.patch('/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}', response_model=DishSchemaOut)
+@router.patch('/{dish_id}', response_model=DishSchemaOut)
 async def update(
         menu_id: UUID,
         submenu_id: UUID,
@@ -43,7 +41,7 @@ async def update(
     return await dish_svc.update(menu_id, submenu_id, dish_id, dish_data)
 
 
-@router.delete('/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}')
+@router.delete('/{dish_id}')
 async def delete(menu_id: UUID, submenu_id: UUID, dish_id: UUID, dish_svc: DishesService = Depends()):
     await dish_svc.delete(menu_id, submenu_id, dish_id)
 
--- app/routers/submenus.py
+++ app/routers/submenus.py
@@ -8,25 +8,25 @@ from app.dependencies import valid_submenu
 from app.schemas import SubmenuSchemaIn, SubmenuSchemaOut
 from app.services import SubmenuService
 
-router = APIRouter(prefix='/menus', tags=['submenus'])
+router = APIRouter(prefix='/menus/{menu_id}/submenus', tags=['submenus'])
 
 
-@router.get('/{menu_id}/submenus', response_model=list[SubmenuSchemaOut])
+@router.get('', response_model=list[SubmenuSchemaOut])
 async def get_list(menu_id: UUID, submenu_svc: SubmenuService = Depends()):
     return await submenu_svc.get_list(menu_id)
 
 
-@router.get('/{menu_id}/submenus/{submenu_id}', response_model=SubmenuSchemaOut)
+@router.get('/{submenu_id}', response_model=SubmenuSchemaOut)
 async def get(submenu: Mapping = Depends(valid_submenu)):
     return submenu
 
 
-@router.post('/{menu_id}/submenus', response_model=SubmenuSchemaOut, status_code=status.HTTP_201_CREATED)
+@router.post('', response_model=SubmenuSchemaOut, status_code=status.HTTP_201_CREATED)
 async def create(menu_id: UUID, submenu_data: SubmenuSchemaIn, submenu_svc: SubmenuService = Depends()):
     return await submenu_svc.create(menu_id, submenu_data)
 
 
-@router.patch('/{menu_id}/submenus/{submenu_id}', response_model=SubmenuSchemaOut)
+@router.patch('/{submenu_id}', response_model=SubmenuSchemaOut)
 async def update(
         menu_id: UUID,
         submenu_id: UUID,
@@ -36,7 +36,7 @@ async def update(
     return await submenu_svc.update(menu_id, submenu_id, submenu_data)
 
 
-@router.delete('/{menu_id}/submenus/{submenu_id}')
+@router.delete('/{submenu_id}')
 async def delete(menu_id: UUID, submenu_id: UUID, submenu_svc: SubmenuService = Depends()):
     await submenu_svc.delete(menu_id, submenu_id)
 
