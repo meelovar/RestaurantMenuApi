8ad7ec43a09adaa269029f98812a6eda652ea19e
meelovar <104781245+meelovar@users.noreply.github.com>
2023-08-14 13:24:48 +0300

    Renamed test

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -3,10 +3,10 @@ from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
-async def test_counts_menu_get(client: AsyncClient, dishes_counts_fixture: dict[str, str]):
+async def test_catalog_get(client: AsyncClient, dishes_counts_fixture: dict[str, str]):
     response = await client.get('/catalog')
     response_json = response.json()
 
-    assert len(response_json) == 2
+    assert len(response_json) == 1
     assert len(response_json[0]['submenus']) == 1
     assert len(response.json()[0]['submenus'][0]['dishes']) == 2
